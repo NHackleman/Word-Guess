34838c1eeb43975c3b449a174860dada0ddeb78f
Nathan <hacklemannathan@gmail.com>
2021-04-11 13:57:59 -0500

    Added ability to choose length of word

--- main.py
+++ main.py
@@ -1,61 +1,81 @@
 import random
+from bs4 import BeautifulSoup
+import requests
 
 # Variable declarations
-word_list = ['horse', 'truck', 'cloud']
-current_word = random.choice(word_list)
+word_list = []
 blank_word_list = []
 blank_word = ''
 correct_choice = False
-lives = 3
+lives = 5
 has_char = False
 
-# Display blank word
-for x in range(1, len(current_word) + 1):
-    blank_word_list.append('_')
+user_input = input('How long would you like the word to be? ')
 
-# While the word and the blank word are not equal
-while current_word != ''.join(blank_word_list):
-    # Resets has_char variable
-    has_char = False
+try:
+    URL = f"http://www.yougowords.com/{user_input}-letters"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    word_table = soup.find(id='sortable-display')
+    results = word_table.find_all('a')
 
-    # Checks if player is out of lives
-    if lives == 0:
-        print('You ran out of lives...')
-        break
+    for result in results:
+        word_list.append(result.text.lower())
 
-    blank_word = ''.join(blank_word_list)
-    print(blank_word)
-    print('\nGuess a letter...')
-    print(f'Lives left: {lives}\n')
+    current_word = random.choice(word_list)
 
-    user_input = input()
-    
-    # Checks to see if user only guessed one letter
-    if len(user_input) > 1:
-        print('Please input only one valid letter')
-    else:
-        print()
-        
-        # Stops game
-        if user_input == 'stop':
+    # Display blank word
+    for x in range(1, len(current_word) + 1):
+        blank_word_list.append('_')
+
+    # While the word and the blank word are not equal
+    while current_word != ''.join(blank_word_list):
+        # Resets has_char variable
+        has_char = False
+
+        # Checks if player is out of lives
+        if lives == 0:
+            print('You ran out of lives...')
             break
 
-        # Checks for users input and if found, replaces space in blank word with letter
-        for y in current_word:
-            if user_input == y:
-                blank_word_list[current_word.find(y)] = y
-                print('Correct!')
-                has_char = True
+        blank_word = ''.join(blank_word_list)
+        print(blank_word)
+        print('\nGuess a letter...')
+        print(f'Lives left: {lives}\n')
 
-            else:
-                continue
+        user_input = input()
         
-        # Takes away life for incorrect guess
-        if not has_char:
-            lives -= 1
-            print('Incorrect')
+        # Checks to see if user only guessed one letter
+        if len(user_input) > 1:
+            print('Please input only one valid letter')
+        else:
+            print()
+            
+            # Stops game
+            if user_input == 'stop':
+                break
 
-if lives > 0:
-    print('You Win!!!')
-else:
-    print('You lose...')
+            # Checks for users input and if found, replaces space in blank word with letter
+            for y in current_word:
+                if user_input == y:
+                    blank_word_list[current_word.find(y)] = y
+                    print('Correct!')
+                    has_char = True
+
+                else:
+                    continue
+            
+            # Takes away life for incorrect guess
+            if not has_char:
+                lives -= 1
+                print('Incorrect')
+
+    if lives > 0:
+        print('You Win!!!')
+    else:
+        print('You lose...')
+
+    print(current_word)
+
+except:
+    print('Invalid input')
